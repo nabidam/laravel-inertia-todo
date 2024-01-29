8548b8313e42650e747a5ea24cbe048c63040f7c
nabidam <navidabbaspoor@gmail.com>
2024-01-29 20:53:00 +0330

    tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.blade.php", "./resources/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
